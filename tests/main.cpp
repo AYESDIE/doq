dec8acee430b7c8d3df495040b9b89eab44fb9de
Ayush Chamoli <ayushchamoli8@gmail.com>
2019-10-02 19:58:28 +0530

    doq::qol::02

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,6 +5,7 @@
 
 //#define CATCH_CONFIG_MAIN  // catch.hpp will define main()
 #define CATCH_CONFIG_RUNNER  // we will define main()
+#include <doq.hpp>
 #include "catch.hpp"
 
 int main(int argc, char** argv)
