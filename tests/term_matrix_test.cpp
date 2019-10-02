dfddf8b999ce1c1dc18529cacec97edcafce2815
Ayush Chamoli <ayushchamoli8@gmail.com>
2019-10-02 17:28:36 +0530

    doq::term_matrix

--- tests/term_matrix_test.cpp
+++ tests/term_matrix_test.cpp
@@ -3,11 +3,22 @@
 //
 
 #include "catch.hpp"
-#include "../src/include/term_matrix.hpp"
+#include "../src/doq.hpp"
 
 
 TEST_CASE("TermMatrixTest", "[TermMatrixTest]")
 {
-  doq::term_matrix TM;
-  REQUIRE(true);
+  doq::document D1("data/a_midsummer_nights_dream.txt"),
+                D2("data/hamlet.txt"),
+                D3("data/macbeth.txt"),
+                D4("data/othello.txt"),
+                D5("data/romeo_and_juliet.txt"),
+                D6("data/the_tempest.txt");
+
+  doq::term_matrix<6> TM;
+  TM.addDocument(D1, D2, D3, D4, D5, D6);
+
+
+  TM.stat(TM["Denmark"] | TM["Africa"]);
+  TM.showMatrix();
 }
